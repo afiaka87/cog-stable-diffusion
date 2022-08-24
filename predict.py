8a9b6fdd95b7d62b33460e5988ff954c7a16187c
Andreas Jansson <andreas.s.t.jansson@gmail.com>
2022-08-24 13:55:26 +0000

    Init image and mask

--- predict.py
+++ predict.py
@@ -12,8 +12,7 @@ class Predictor(BasePredictor):
     def setup(self):
         """Load the model into memory to make running multiple predictions efficient"""
         print("Loading pipeline...")
-
-        lms = LMSDiscreteScheduler(
+        scheduler = PNDMScheduler(
             beta_start=0.00085, beta_end=0.012, beta_schedule="scaled_linear"
         )
         self.pipe = StableDiffusionPipeline.from_pretrained(
@@ -25,12 +24,33 @@ class Predictor(BasePredictor):
 
     def predict(
         self,
-        prompt: str = Input(description="Input prompt"),
+        prompt: str = Input(description="Input prompt", default=""),
+        width: int = Input(
+            description="Width of output image",
+            choices=[128, 256, 512, 768, 1024],
+            default=512,
+        ),
+        height: int = Input(
+            description="Height of output image",
+            choices=[128, 256, 512, 768],
+            default=512,
+        ),
+        init_image: Path = Input(
+            description="Inital image to generate variations of", default=None
+        ),
+        mask: Path = Input(
+            description="Black and white image to use as mask for inpainting over init_image. Black pixels are inpainted and white pixels are preserved",
+            default=None,
+        ),
+        prompt_strength: float = Input(
+            description="Prompt strength when using init image. 1.0 corresponds to full destruction of information in init image",
+            default=0.8,
+        ),
         num_outputs: int = Input(
             description="Number of images to output", choices=[1, 4], default=1
         ),
         num_inference_steps: int = Input(
-            description="Number of denoising steps", ge=1, le=500, default=100
+            description="Number of denoising steps", ge=1, le=500, default=50
         ),
         guidance_scale: float = Input(
             description="Scale for classifier-free guidance", ge=1, le=20, default=7.5
@@ -44,10 +64,21 @@ class Predictor(BasePredictor):
             seed = int.from_bytes(os.urandom(2), "big")
         print(f"Using seed: {seed}")
 
-        prompt = [prompt] * num_outputs
+        if init_image:
+            init_image = Image.open(init_image).convert("RGB")
+            init_image = preprocess_init_image(init_image, width, height).to("cuda")
+        if mask:
+            mask = Image.open(mask).convert("RGB")
+            mask = preprocess_mask(mask, width, height).to("cuda")
+
         generator = torch.Generator("cuda").manual_seed(seed)
         output = self.pipe(
-            prompt,
+            prompt=[prompt] * num_outputs if prompt is not None else None,
+            init_image=init_image,
+            mask=mask,
+            width=width,
+            height=height,
+            prompt_strength=prompt_strength,
             guidance_scale=guidance_scale,
             generator=generator,
             num_inference_steps=num_inference_steps,
